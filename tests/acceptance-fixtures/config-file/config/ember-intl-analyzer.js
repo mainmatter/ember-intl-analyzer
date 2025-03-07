bfbb9057ee55d9ce36868cd7fe97300c76eefe1c
Chris Manson <chris@manson.ie>
2025-03-07 11:57:32 +0000

    add a test for loading a config file in all permiatiations

---
+++ tests/acceptance-fixtures/config-file/config/ember-intl-analyzer.js
@@ -0,0 +1,7 @@
+module.exports = {
+  errorOnUnusedWhitelistEntries: false,
+  whitelist: [
+    /some\.whitelisted\.translation-(a|b)/,
+    /some\.unused\.whitelisted\.translation-(a|b)/,
+  ],
+};
